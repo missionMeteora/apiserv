cd3aef19ec6159ee672e9d6618129a8a1a50b873
OneOfOne <oneofone@gmail.com>
2022-04-04 17:16:43 -0500

    set minimum tls to v1.2

--- autocert.go
+++ autocert.go
@@ -18,30 +18,28 @@ import (
 // certCacheDir is where the certificates will be cached, defaults to "./autocert".
 // Note that it must always run on *BOTH* ":80" and ":443" so the addr param is omitted.
 func (s *Server) RunAutoCert(certCacheDir string, domains ...string) error {
+	if certCacheDir == "" {
+		certCacheDir = "./autocert"
+	}
+
+	if err := os.MkdirAll(certCacheDir, 0o700); err != nil {
+		return fmt.Errorf("couldn't create cert cache dir: %v", err)
+	}
+
 	m := &autocert.Manager{
 		Prompt: autocert.AcceptTOS,
+		Cache:  autocert.DirCache(certCacheDir),
 	}
 
 	if len(domains) > 0 {
 		m.HostPolicy = autocert.HostWhitelist(domains...)
 	}
 
-	if certCacheDir == "" {
-		certCacheDir = "./autocert"
-	}
-
-	if err := os.MkdirAll(certCacheDir, 0700); err != nil {
-		return fmt.Errorf("couldn't create cert cache dir: %v", err)
-	}
-
-	m.Cache = autocert.DirCache(certCacheDir)
-
 	srv := s.newHTTPServer(":https")
 
-	srv.TLSConfig = &tls.Config{
-		GetCertificate: m.GetCertificate,
-		NextProtos:     []string{"h2", "http/1.1"}, // Enable HTTP/2
-	}
+	tlsCfg := m.TLSConfig()
+	tlsCfg.MinVersion = tls.VersionTLS12
+	srv.TLSConfig = tlsCfg
 
 	s.serversMux.Lock()
 	s.servers = append(s.servers, srv)
@@ -118,7 +116,7 @@ func (s *Server) RunTLSAndAuto(certCacheDir string, certPairs []CertPair, hosts 
 		certCacheDir = "./autocert"
 	}
 
-	if err := os.MkdirAll(certCacheDir, 0700); err != nil {
+	if err := os.MkdirAll(certCacheDir, 0o700); err != nil {
 		return fmt.Errorf("couldn't create cert cache dir (%s): %v", certCacheDir, err)
 	}
 
